ca2a5f7b6fa8922c3318c7fbc6e16ef870af627e
Stephen <hongwei5489@gmail.com>
2021-09-19 17:50:21 +0800

    update

---
+++ uploads/fsec2021/droidcheck/solve.py
@@ -0,0 +1,20 @@
+transform_table = bytes.fromhex("c649139a6709de2b581e48534f9d35ae81d8c477ad96c1ee0c16321faa08e5ca8783fe45e01454ff5e107fd3202d2ea77b3e64a2846f91bfb441d6ef75aced5b3c50740f045d714b25ba9f3fe1608c33e7c7f41bc5bce2ecb3b143231a9c247ecdda826cd038707d0afd01114e7a97ce408826b7a086cb1799306e63988accd2025a56348ba4807c19429521b9c28e6690550d47b6e4d9d4a18d93db6d92361261f0e3f573f1c9c872c0f2aba885f8afd52ff90beb9e4cdc94bbd1a6298f374aa35122e939e6c31c0076523b65fb0344f305a95c46e857f74d3d0627cf153af65fddb8b2fc68d7bd629b0789592a6b31a51d0eb5282cfabe79186a7869eab0")
+target = [i%256 for i in [-74, 56, -99, -111, 95, 98, -38, -116, -5, 76, -18, -84, -65, -112, 31, -81]]
+
+flag = bytearray(b'a'*16)
+b2 = 0
+for i in range(0,len(target),2):
+	b1 = transform_table[i]
+	if transform_table[flag[i]] ^ b1 ^ b2 != target[i]:
+		for j in range(256):
+			if transform_table[j] ^ b1 ^ b2 == target[i]:
+				flag[i] = j
+				break
+
+	b2 = transform_table[i + 1] ^ b1 ^ b2;
+	if transform_table[flag[i + 1]] ^ b2 != target[i+1]:
+		for j in range(256): 
+			if transform_table[j] ^ b2 == target[i+1]:
+				flag[i+1] = j
+				break
+print(flag)
